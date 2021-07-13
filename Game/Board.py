dadcb4003d23a5c6e3d03f5ff89cb41e75fe44e5
Yasha Klebanov <yklebanov02@gmail.com>
2021-07-13 13:03:41 -0400

    Continued on main.py and relative classes

--- Game/Board.py
+++ Game/Board.py
@@ -1,19 +1,33 @@
 # Board.py - Prints the board, showing the numbers of the positions
 
-board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-def print_positions():
-    print(str(positions[0]) + " | " + str(positions[1]) + " | " + str(positions[2]))
-    print("---------")
-    print(str(positions[3]) + " | " + str(positions[4]) + " | " + str(positions[5]))
-    print("---------")
-    print(str(positions[6]) + " | " + str(positions[7]) + " | " + str(positions[8]))
+# positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+class Board:
+    board = None
+    # Constructor Class --> Set Board to Default
+    def __init__(self):
+        self.board = [' ', ' ', ' ',
+                     ' ', ' ', ' ',
+                     ' ', ' ', ' ']
 
-# Prints the board, showing the current state of X, O, and blanks
-def print_board(place, symbol):
-    board[place] = symbol
-    print(str(board[0]) + " | " + str(board[1]) + " | " + str(board[2]))
-    print("---------")
-    print(str(board[3]) + " | " + str(board[4]) + " | " + str(board[5]))
-    print("---------")
-    print(str(board[6]) + " | " + str(board[7]) + " | " + str(board[8]))
+    def setToken(self, pos, token):
+        if self.__checkPos(pos):
+            self.board[pos-1] = token
+            return True
+        else:
+            return False
+
+    def getToken(self, pos):
+        return self.board[pos-1]
+
+    def __checkPos(self, pos):
+        if pos == ' ':
+            return True
+
+    # Prints the board, showing the current state of X, O, and blanks
+    def print_board(self):
+    # board[place] = symbol
+        print(str(self.board[0]) + " | " + str(self.board[1]) + " | " + str(self.board[2]))
+        print("---------")
+        print(str(self.board[3]) + " | " + str(self.board[4]) + " | " + str(self.board[5]))
+        print("---------")
+        print(str(self.board[6]) + " | " + str(self.board[7]) + " | " + str(self.board[8]))
--- Game/main.py
+++ Game/main.py
@@ -1,12 +1,42 @@
 # Project Name: Tic-Tac-Toe
 # Author: Yasha Klebanov
-from Board import print_positions, print_board
-board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-# Game Loop
-player = "X"
+from .Board import Board
+currentBoard = Board()
+# Validates user's input
+def valid_input(input):
+    if input.isdigit() and (1 <= int(input) < 10):
+        return True
+    else:
+        return False
+
+# Main game Loop
 game_over = False
-print_positions()
+while game_over == False:
+    # Player X's Turn
+    player = "X"
+    user_input = 0
+    flag = True
+    while flag:
+        user_input = input("Where do you want to move? ")
+        if valid_input(user_input):
+            flag = False
+    user_input = int(user_input)
+    currentBoard.setToken(user_input, player)
+    currentBoard.print_board()
+    #board[user_input-1] = player
+    #print_board()
 
-
+    # Player O's Turn
+    player = "O"
+    user_input = 0
+    flag = True
+    while flag:
+        user_input = input("Where do you want to move? ")
+        if valid_input(user_input):
+            flag = False
+    user_input = int(user_input)
+    currentBoard.setToken(user_input, player)
+    currentBoard.print_board()
+    #board[user_input - 1] = player
+    #print_board()
